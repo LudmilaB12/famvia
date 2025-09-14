dc93423317cf20f24c2d6042c10b25206436475a
LudmilaB12 <ludmilabalbiserrentino@gmail.com>
2025-09-14 17:20:38 -0300

    feat: add deco SVG files and update styles for the homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 // import Navbar from "@/components/ui/Navbar/Navbar";
 // import Navbar from "../../components/ui/Navbar/Navbar";
 import Navbar from "@/src/components/ui/Navbar/Navbar"
@@ -7,9 +7,14 @@ import Navbar from "@/src/components/ui/Navbar/Navbar"
 export default function Home() {
   return (
     <div >
-      <main >
+      <main className={styles.bgr}> 
         <Navbar />
-        
+        <div className={styles.description}>
+          <h1>Meet our Travel Advisors
+            and find your perfect match!</h1>
+          <h3>Ready to actually enjoy your next family vacation?</h3>
+        </div>
+
       </main>
       <footer>
       </footer>
